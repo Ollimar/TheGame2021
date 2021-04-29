45acfb6256d65e6964ec8d18bbd531b8d9ed2895
Olli-Matti Rautiainen <ollimar84@gmail.com>
2021-04-29 16:02:53 +0300

    Day 169 part 2
    Tongue works again

--- TheGame/Assets/Scripts/Tongue.cs
+++ TheGame/Assets/Scripts/Tongue.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Tongue : MonoBehaviour
 {
+    public bool tongueActive = false;
+
     private PlayerScript player;
+    public GameObject playerObject;
 
     public GameObject tongueStart;
     public GameObject tongueEnd;
 
     public GameObject tonguePosition;
     public GameObject attachedObject;
+
+    public ParticleSystem launchPuff;
 
     public float tongueSpeed = 5f;
     public float lickDuration = 0.5f;
@@ -25,16 +30,30 @@ public class Tongue : MonoBehaviour
         tongueEnd = GameObject.Find("TongueMaximum");
         tonguePosition = GameObject.Find("TonguePosition");
         player = GameObject.Find("Player").GetComponent<PlayerScript>();
+        playerObject = GameObject.Find("Player");
+        launchPuff = GameObject.Find("LaunchParticle").GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        transform.position = Vector3.Lerp(transform.position,tonguePosition.transform.position, tongueSpeed*Time.deltaTime);
-
-        if(Input.GetButtonDown("Fire1"))
+        if(!attached)
         {
+            transform.position = Vector3.Lerp(transform.position, tonguePosition.transform.position, tongueSpeed * Time.deltaTime);
+        }
+
+        if(attached)
+        {
+            player.canMove = false;
+            launchPuff.Play();
+            playerObject.GetComponent<Rigidbody>().useGravity = false;
+            playerObject.GetComponent<Rigidbody>().isKinematic = true;
+            playerObject.transform.position = Vector3.Lerp(playerObject.transform.position,transform.position,10f*Time.deltaTime);
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            tongueActive = true;
             player.canMove = false;
             tonguePosition.transform.position = tongueEnd.transform.position;
             StartCoroutine("Return");
@@ -45,9 +64,38 @@ public class Tongue : MonoBehaviour
     {
         if(other.gameObject.tag == "Enemy")
         {
-            tonguePosition.transform.position = tongueEnd.transform.position;
+            tonguePosition.transform.position = tongueStart.transform.position;
             attachedObject = other.gameObject;
             Destroy(other.gameObject);
+        }
+
+        if(other.gameObject.tag == "Untagged")
+        {
+            tonguePosition.transform.position = tongueStart.transform.position;
+        }
+
+        if(other.gameObject.tag == "AttachPoint")
+        {
+            if(tongueActive)
+            {
+                attached = true;
+                tonguePosition.transform.position = other.gameObject.transform.position;
+            }
+
+        }
+
+        if(other.gameObject.tag == "Player")
+        {
+            if(attached)
+            {
+                launchPuff.Stop();
+                player.canMove = true;
+                playerObject.GetComponent<Rigidbody>().useGravity = true;
+                playerObject.GetComponent<Rigidbody>().isKinematic = false;
+                attached = false;
+                tongueActive = false;
+                tonguePosition.transform.position = tongueStart.transform.position;
+            }
         }
     }
 
@@ -56,8 +104,8 @@ public class Tongue : MonoBehaviour
         yield return new WaitForSeconds(lickDuration);
         tonguePosition.transform.position = tongueStart.transform.position;
         player.canMove = true;
-
-        if(attachedObject != null)
+        tongueActive = false;
+        if (attachedObject != null)
         {
             Destroy(attachedObject);
         }
