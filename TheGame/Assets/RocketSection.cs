3d320c752454f8ac2e11a24302dce6acdf75d061
Olli-Matti Rautiainen <ollimar84@gmail.com>
2022-07-04 01:16:08 +0300

    Commit040722
    Updates to blocktown and iceplanet levels

--- TheGame/Assets/RocketSection.cs
+++ TheGame/Assets/RocketSection.cs
@@ -8,11 +8,28 @@ public class RocketSection : MonoBehaviour
     public float speed = 5f;
     public float limit = 610.55f;
 
+    public Vector3 trainDirection;
+    public int direction;
+
     public bool playerOn = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        switch (direction)
+        {
+            case 3:
+                trainDirection = new Vector3(0, 0, 1);
+                break;
+            case 2:
+                trainDirection = new Vector3(1, 0, 0);
+                break;
+            case 1:
+                trainDirection = new Vector3(0, 0, -1);
+                break;
+            default:
+                trainDirection = new Vector3(-1, 0, 0);
+                break;
+        }
     }
 
     // Update is called once per frame
@@ -20,7 +37,7 @@ public class RocketSection : MonoBehaviour
     {
         if (playerOn && transform.position.z < limit)
         {
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            transform.Translate(trainDirection * speed * Time.deltaTime);
         }
     }
 
---
+++ TheGame/Assets/Scripts/MovingTrain.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingTrain : MonoBehaviour
+{
+    public int movementDirection;
+    public Transform startPoint;
+    public Vector3 direction;
+
+    public float speed = 2f;
+
+    private float pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (movementDirection)
+        {
+            case 3:
+                direction = new Vector3(0, 0, 1);
+                break;
+            case 2:
+                direction = new Vector3(1, 0, 0);
+                break;
+            case 1:
+                direction = new Vector3(0, 0, -1);
+                break;
+            default:
+                direction = new Vector3(-1, 0, 0);
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "TrainPumber")
+        {
+            transform.position = startPoint.position;
+        }
+    }
+}
