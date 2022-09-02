21a8b1732657b77fa1d606823eff9911215ed9e2
Olli-Matti Rautiainen <ollimar84@gmail.com>
2022-09-02 15:33:20 +0300

    Commit020922
    Added Opening Cutscene

---
+++ TheGame/Assets/Scripts/OpeningCutsceneManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OpeningCutsceneManager : MonoBehaviour
+{
+    public GameObject[] cameras;
+
+    public GameObject cabin;
+
+    public GameObject[] tipus;
+
+    public GameObject theTipu;
+
+    public GameObject villain;
+
+    public float timer;
+
+    public GameObject fade;
+
+    public AudioSource audioSource;
+    public AudioClip[] music;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource.clip = music[0];
+        audioSource.Play();
+        StartCoroutine("Scene1");
+        
+        for (int i=0; i< tipus.Length;i++)
+        {
+            tipus[i].GetComponent<TipuToRescue>().tears[0].Stop();
+            tipus[i].GetComponent<TipuToRescue>().tears[1].Stop();
+        }
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            StartCoroutine("LoadMenu");
+        }
+    }
+
+    public IEnumerator Scene1()
+    {
+        yield return new WaitForSeconds(5f);
+        cabin.GetComponent<Animator>().SetBool("DoorOpen", true);
+        audioSource.clip = music[1];
+        audioSource.Play();
+        tipus[0].GetComponent<TipuToRescue>().Running();       
+        tipus[0].GetComponent<TipuToRescue>().small = false;       
+        yield return new WaitForSeconds(2f);
+        tipus[1].GetComponent<TipuToRescue>().Running();
+        tipus[1].GetComponent<TipuToRescue>().small = false;
+        yield return new WaitForSeconds(2f);
+        tipus[2].GetComponent<TipuToRescue>().Running();
+        tipus[2].GetComponent<TipuToRescue>().small = false;
+        yield return new WaitForSeconds(2f);
+        tipus[3].GetComponent<TipuToRescue>().Running();
+        tipus[3].GetComponent<TipuToRescue>().small = false;
+        yield return new WaitForSeconds(2f);
+        tipus[4].GetComponent<TipuToRescue>().Running();
+        tipus[4].GetComponent<TipuToRescue>().small = false;
+        yield return new WaitForSeconds(2f);
+        tipus[5].GetComponent<TipuToRescue>().Running();
+        tipus[5].GetComponent<TipuToRescue>().small = false;
+        yield return new WaitForSeconds(3f);
+        theTipu.GetComponent<TipuToRescue>().Running();
+        theTipu.GetComponent<TipuToRescue>().eyes[0].SetActive(true);
+        theTipu.GetComponent<TipuToRescue>().eyes[1].SetActive(false);
+        theTipu.GetComponent<TipuToRescue>().small = false;
+        yield return new WaitForSeconds(1f);
+        theTipu.GetComponent<TipuToRescue>().Stop();
+        yield return new WaitForSeconds(1f);
+        theTipu.GetComponent<TipuToRescue>().Wave();
+        theTipu.GetComponent<TipuToRescue>().eyes[0].SetActive(false);
+        theTipu.GetComponent<TipuToRescue>().eyes[1].SetActive(true);
+    }
+
+    public IEnumerator LoadMenu()
+    {
+        fade.GetComponent<Animator>().SetTrigger("FadeOut");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene("HomeLevel");
+    }
+}
--- TheGame/Assets/Scripts/TipuToRescue.cs
+++ TheGame/Assets/Scripts/TipuToRescue.cs
@@ -4,23 +4,68 @@ using UnityEngine;
 
 public class TipuToRescue : MonoBehaviour
 {
+    public bool inCutScene = true;
+
     private Rigidbody myRB;
     private Animator myAnim;
 
     public GameObject[] eyes;
     public ParticleSystem[] tears;
 
+    public bool isRunning = false;
+
+    public float speed = 1f;
+    public float newRotation;
+
+    public bool small = true;
+    public float scaleSpeed = 1.1f;
+
     // Start is called before the first frame update
     void Start()
     {
+        speed = 0f;
         myAnim = GetComponent<Animator>();
         myRB = GetComponent<Rigidbody>();
+
+        // Set the caharacter initial size to small so it can walk through the door
+        if (small && inCutScene)
+        {
+            transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        if(!small && inCutScene)
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(0.75f, 0.75f, 0.75f), scaleSpeed * Time.deltaTime);
+        }
+    }
+
+    public void Running()
+    {
+        myAnim.SetBool("Running", true);
+        if(tears[0] != null)
+        {
+            tears[0].Stop();
+            tears[1].Stop();
+            eyes[0].SetActive(false);
+            eyes[1].SetActive(true);
+        }
+        speed = 5f;
+    }
+
+    public void Stop()
+    {
+        speed = 0f;
+        myAnim.SetBool("Running", false);
+    }
+
+    public void Wave()
+    {
+        myAnim.SetBool("Wave", true);
     }
 
     public void Rescued()
@@ -32,5 +77,13 @@ public class TipuToRescue : MonoBehaviour
         tears[1].Stop();
         eyes[0].SetActive(false);
         eyes[1].SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "RotatorTrigger")
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x,newRotation,transform.eulerAngles.z);
+        }
     }
 }
